2947e5107f2d117cea760f89c15d667d406e3231
student <student@imac4.labs.cs.kent.edu>
2017-04-11 11:47:01 -0400

    added new cell for submit/cancel in manage progress screen

--- DailyGoalTracker/ManageProgressViewController.swift
+++ DailyGoalTracker/ManageProgressViewController.swift
@@ -13,6 +13,8 @@ class ManageProgressViewController: UITableViewController, HasMainMenuProtocol {
     
     @IBOutlet weak var goalsTitleLabel: UILabel!
     
+    let sectionCount = 2
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -26,25 +28,45 @@ class ManageProgressViewController: UITableViewController, HasMainMenuProtocol {
     // MARK: - Table view data source
     
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return sectionCount
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return (mainMenuVC?.goalList.count)!
+        switch section {
+        case 0:
+            return (mainMenuVC?.goalList.count)!
+        default:
+            return 1
+        }
     }
     
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! ManageProgressTableViewCell
-        
-        // Configure the cell...
-        cell.goalLabel.text = (mainMenuVC?.goalList[indexPath.row].0)!
-        cell.goalAccomplished.isOn = (mainMenuVC?.goalList[indexPath.row].1)!
 
-        // set switch tag as index row
-        cell.goalAccomplished.tag = indexPath.row
-        
-        return cell
+        switch indexPath.section {
+        case 0:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! ManageProgressTableViewCell
+            // Configure the cell...
+            cell.goalLabel.text = (mainMenuVC?.goalList[indexPath.row].0)!
+            cell.goalAccomplished.isOn = (mainMenuVC?.goalList[indexPath.row].1)!
+            
+            // set switch tag as index row
+            cell.goalAccomplished.tag = indexPath.row
+            
+            return cell
+        default:
+            //old
+            let cell = tableView.dequeueReusableCell(withIdentifier: "FinalCell", for: indexPath) as! ManageProgressFinalTableViewCell
+//            // Configure the cell...
+//            cell.goalLabel.text = (mainMenuVC?.goalList[indexPath.row].0)!
+//            cell.goalAccomplished.isOn = (mainMenuVC?.goalList[indexPath.row].1)!
+//            
+//            // set switch tag as index row
+//            cell.goalAccomplished.tag = indexPath.row
+            
+            return cell
+        }
+
     }
     
     @IBAction func toggleGoalSwitch(_ sender: UISwitch) {
